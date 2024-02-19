e24ea47378f405aa6ce0d3cc39bf62e4b6ec0454
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-02-19 15:16:31 +0800

    adjustments/frontend

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ async function startServer () {
   const app = express()
   const connection = await pool.connect()
 
-  app.use(express.json());
+  app.use(express.json())
 
   app
     .post('/pogs', async (request, response) => {
@@ -18,51 +18,57 @@ async function startServer () {
         response.json(newPogs.rows)
       } catch (err) {
         console.log('error', err)
-        response.status(500).json({ error: '' })
+        response.status(500).json({ error: 'failed to add pogs' })
       }
     })
-    .get('/pogs', async ( _, response) => {
-        try {
-            const result = await connection.query('SELECT * FROM pogs')
-            response.status(200).json(result.rows)
-        }
-        catch (err) {
-            console.log('error', err)
-            response.status(404).send('Not  Found')
-        }
+    .get('/pogs', async (_, response) => {
+      try {
+        const result = await connection.query('SELECT * FROM pogs')
+        response.status(200).json(result.rows)
+      } catch (err) {
+        console.log('error', err)
+        response.status(404).send('Not  Found')
+      }
     })
     .get('/pogs/:id', async (request, response) => {
-        try {
-            const id = request.params.id
-            const result = await connection.query('SELECT * FROM pogs WHERE id = $1', [id])
-            response.status(200).json(result.rows)
-        }
-        catch(err) {
-            console.log('error', err)
-            response.status(404).send('Not found')
-        }
+      try {
+        const id = request.params.id
+        const result = await connection.query(
+          'SELECT * FROM pogs WHERE id = $1',
+          [id]
+        )
+        response.status(200).json(result.rows)
+      } catch (err) {
+        console.log('error', err)
+        response.status(404).send('Not found')
+      }
     })
     .put('/pogs/:id', async (request, response) => {
-        try {
-            const {name, ticker_symbol, price, color} = request.body
-            const result = await connection.query('UPDATE pogs SET name = $1, ticker_symbol = $2, price = $3, color = $4 WHERE id = $5 RETURNING *',
-            [name, ticker_symbol, price, color, request.params.id])
-            response.status(200).json(result.rows)
-        }catch (err) {
-            console.log('error', err)
-            response.status(404).send('not found')
-        }
+      try {
+        const { name, ticker_symbol, price, color } = request.body
+        const result = await connection.query(
+          'UPDATE pogs SET name = $1, ticker_symbol = $2, price = $3, color = $4 WHERE id = $5 RETURNING *',
+          [name, ticker_symbol, price, color, request.params.id]
+        )
+        response.status(200).json(result.rows)
+      } catch (err) {
+        console.log('error', err)
+        response.status(404).send('not found')
+      }
     })
-    .delete('/pogs/:id', async ( request, response) => {
-        try {
-            const result = await connection.query('DELETE FROM pogs WHERE id = $1 RETURNING *', [request.params.id]);
-    response.status(200).json(result.rows);
-        } catch (err) {
-            console.log('error', err) 
-            response.status(404).send('Not found')
-        }
-    } )
-    .use(express.static('frontend')) 
+    .delete('/pogs/:id', async (request, response) => {
+      try {
+        const result = await connection.query(
+          'DELETE FROM pogs WHERE id = $1 RETURNING *',
+          [request.params.id]
+        )
+        response.status(200).json(result.rows)
+      } catch (err) {
+        console.log('error', err)
+        response.status(404).send('Not found')
+      }
+    })
+    .use(express.static('frontend'))
     .listen(3000, () => {
       console.log('carlo mbappe http://localhost:3000')
     })
